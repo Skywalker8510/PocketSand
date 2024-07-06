9398d436dbdf596de763b42284c74e8d90abcaf8
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2024-07-06 10:49:20 -0500

    Initial commit

---
+++ src/main/java/net/skywalker8510/pocketsand/PocketSandDataGenerator.java
@@ -0,0 +1,11 @@
+package net.skywalker8510.pocketsand;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PocketSandDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
